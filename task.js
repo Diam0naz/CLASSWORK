8e85710595aa2db52818730c06fcb0e5494ba6a5
Diam0naz <jutenoh@gmail.com>
2025-04-15 10:41:32 +0000

    Adds Task

---
+++ task.js
@@ -0,0 +1,64 @@
+/*
+1. Given two user profile objects (e.g., personalInfo and contactInfo), merge them into a single userProfile object.
+2. Given a product object, log all its keys and values. Bonus: Format them as "key: value" pairs in an array or print them line by line.
+3. Create a settings object (e.g., theme: "dark", notifications: true). Freeze it and try changing a value. Use Object.isFrozen to confirm if it's immutable.
+*/
+
+
+// Task One
+let personalInfo = {
+    firsrName: "Matthew",
+    surName: "Kuru"
+}
+
+let contactInfo = {
+    location: "Plateau State",
+    residence: "Ray-field",
+    phone: "+234 8367221372" 
+}
+
+const userProfile = Object.assign(personalInfo, contactInfo)
+
+//console.log(userProfile)
+
+// Task Two
+const shopProduct = {
+    kitchenUtendils: ["Spoon", "Plates", "Cups"],
+    foodStuffs: ["Yam", "Garri", "Onions", "Rice" ],
+    grocries: [ "Matches", "Candles", "Milo", "Bournvita" ]
+}
+
+//logging both keys and values 
+console.log(shopProduct)
+
+//logging only the keys
+const keys = Object.keys(shopProduct)
+console.log(keys)
+
+
+//logging only the values
+const values = Object.values(shopProduct)
+console.log(values)
+
+function productChecker(product) {
+    if (shopProduct.includes(product)){
+        console.log(`Product is in ${shopProduct.foodStuffs}`)
+    }
+}
+
+// Task Three
+const settings = {
+    theme: "dark",
+    notification: true
+}
+
+Object.freeze(settings)
+settings.theme = "light"
+
+if (Object.isFrozen(settings) == true) {
+    console.log("Immutable settings!")
+} else {
+    console.log("Mutable")
+}
+
+console.log(settings)
